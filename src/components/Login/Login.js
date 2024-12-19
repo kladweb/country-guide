2d8c0090cd28dca09fa4ad097a1b312d43387d3e
Pavel Kladkevich <kladpav@gmail.com>
2024-12-19 23:14:11 +0300

    fix: correct styles properties for login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,8 +4,8 @@ import './login.css';
 export const Login = ({loginGoogle}) => {
   return (
     <button className='loginButton' onClick={loginGoogle}>
-      <img src="/img/shared/google_icon.png" alt="google icon" />
-      <span>Login with Google</span>
+      <img className='google-logo' src="/img/shared/google_icon.png" alt="google icon" />
+      <span className='google-span'>Login with Google</span>
     </button>
   )
 }
