59ddb514e81579cf268180810360d07c7175033d
Pavel Kladkevich <kladpav@gmail.com>
2025-03-08 18:47:38 +0300

    refactor: Rewrite scss for CountryInfo

--- src/components/CountryInfo/CountryInfo.tsx
+++ src/components/CountryInfo/CountryInfo.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from "react-router-dom";
-import './CountryInfo.css';
+import './CountryInfo.scss';
 import { ICountries } from "../../types/globalTypes";
 import { useAppSelector } from "../../redux/store";
 
